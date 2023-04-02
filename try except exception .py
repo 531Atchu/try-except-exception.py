77b7f4e12813b0dd74e38f03a6ab62f7b00dc0c6
531Atchu <125687864+531Atchu@users.noreply.github.com>
2023-04-02 21:41:42 +0530

    Add files via upload

---
+++ try except exception .py
@@ -0,0 +1,9 @@
+print ("Enter the first number")
+num1=input()
+print ("Enter the second number")
+num2=input()
+try:
+    print("the sum of two number is",int (num1)+int(num2))
+except Exception as k:
+    print(K)
+    print("this is a very important line")
